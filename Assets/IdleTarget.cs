5b96678a580943ad70c18fce32df665146e1a1cf
Retsav <ddadanski@edu.cdv.pl>
2023-05-25 06:59:51 +0200

    IdlePoint check if agent is standing

--- Assets/IdleTarget.cs
+++ Assets/IdleTarget.cs
@@ -6,17 +6,31 @@ using UnityEngine;
 public class IdleTarget : MonoBehaviour
 {
     [SerializeField] private GameObject player;
+    private ExhaustionHandler exhaustionHandler;
+    public bool isResting;
 
     private void Awake()
     {
         player = GameObject.FindWithTag("Player");
+        exhaustionHandler = player.GetComponent<ExhaustionHandler>();
     }
 
     private void Update()
     {
+        CheckIfResting();
+    }
+
+    private void CheckIfResting()
+    {
         if (player.transform.position.x == transform.position.x)
         {
-            Debug.Log("Dodaj Exhaustion");
+            isResting = true;
+            if (exhaustionHandler.exhaustionRemaining >= exhaustionHandler.exhaustionMax) return;
+            exhaustionHandler.exhaustionRemaining += Time.deltaTime * exhaustionHandler.restMultiplier;
+        }
+        else
+        {
+            isResting = false;
         }
     }
 }
