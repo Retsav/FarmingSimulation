8351fa0acd72fc38b10f70e9c950a707d4e8dc3e
Retsav <ddadanski@edu.cdv.pl>
2023-05-28 16:49:52 +0200

    Seeding system

--- Assets/Scripts/PlantHandler.cs
+++ Assets/Scripts/PlantHandler.cs
@@ -1,18 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlantHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private List<Transform> plantSpaces;
+    private GameObject player;
+    [SerializeField] private Image actionfillBar;
+    [SerializeField] private GameObject actionBar;
+
+    [SerializeField] private float timeBeforeSeeded;
+    [SerializeField] private float maxTimeSeed = 5f;
+    
+    public delegate void InformSeed(Transform plantSpace);
+    public static InformSeed informSeed;
+    public static InformSeed informDoneSeeding;
+
+    private void Awake()
     {
-        
+        player = GameObject.FindWithTag("Player");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        CheckIfEmpty();
+        foreach (var plant in plantSpaces)
+        {
+            if (!plant.GetChild(0).GameObject().activeSelf)
+            {
+                if (player.transform.position.x == plant.transform.position.x)
+                {
+                    actionBar.SetActive(true);
+                    if (maxTimeSeed >= timeBeforeSeeded)
+                    {
+                        timeBeforeSeeded += Time.deltaTime;
+                    } else  {
+                        informDoneSeeding?.Invoke(plant.GameObject().transform);
+                        plant.GetChild(0).GameObject().SetActive(true);
+                        timeBeforeSeeded = 0f;
+                        actionBar.SetActive(false);
+                    }
+                    actionfillBar.fillAmount = GetSeedTimeNormalized();
+                }
+                else
+                {
+                    actionBar.SetActive(false);
+                }
+            }
+        }
+    }
+
+    private float GetSeedTimeNormalized()
+    {
+        return timeBeforeSeeded / maxTimeSeed;
+    }
+
+    private void CheckIfEmpty()
+    {
+        foreach (var plant in plantSpaces)
+        {
+            if (plant.GetChild(0).GameObject().activeSelf) return;
+            Debug.Log("Wykryto seedplace bez rosliny");
+            informSeed?.Invoke(plant.GameObject().transform);
+        }
     }
 }
